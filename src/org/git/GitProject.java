4dd03549cad6d46253387d6b3424583a7b0ba7b4
SaraswathiC <saramba06@gmail.com>
2022-03-10 10:44:42 +0530

    Task done

---
+++ src/org/git/GitProject.java
@@ -0,0 +1,13 @@
+package org.git;
+
+public class GitProject {
+	private void test() {
+		System.out.println("test");
+
+	}
+	public static void main(String[] args) {
+		GitProject g = new GitProject();
+		g.test();
+	}
+
+}
